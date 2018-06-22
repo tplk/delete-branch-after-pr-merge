ce8caaa9351b9bb5443b6cdbe7ca4c71bb0dcfba
Dmitry Teplov <siht.pilf@gmail.com>
2018-06-22 23:28:55 +0300

    Release 0.0.1.

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# probot-delete-branch-after-pr-merge
-
-> a GitHub App built with [probot](https://github.com/probot/probot) that automatically deletes branch after PR is merged.
+# delete-branch-after-pr-merge
+> a GitHub App built with [probot](https://github.com/probot/probot)
+that automatically deletes branch when Pull Request is merged.
 
 ## Setup
+Create a `.github/delete-branch-after-pr-merge.yml` file
+in default branch of your repository after bot installation via
+[GitHub Apps](https://github.com/marketplace/delete-branch-after-pr-merge).
 
+#### Default configuration:
+```yaml
+# An array of excluded branches which shouldn't be deleted after pr merge.
+exclude:
+ - master
+# If set to true bot will leave a comment after deleting merged branch.
+comment: true
 ```
-# Install dependencies
-npm install
-
-# Run the bot
-npm start
-```
-
-See [docs/deploy.md](docs/deploy.md) if you would like to run your own instance of this app.
--- index.js
+++ index.js
@@ -1,16 +1,54 @@
+const getConfig = require('./lib/get-config')
+const defaultConfig = require('./lib/default-config')
+
+const deleteBranch = async (context, head) =>
+  context.github.gitdata.deleteReference(head)
+
+const setupConfig = async (context) => {
+  let config = await getConfig(context, 'delete-branch-after-pr-merge.yml', defaultConfig)
+
+  if (config == null) {
+    config = {...defaultConfig}
+  } else if (config.exclude == null || config.exclude.length == null) {
+    config.exclude = [...defaultConfig.exclude]
+  }
+
+  return config
+}
+
 module.exports = function (robot) {
-  robot.log('Yay, the app was loaded!')
-
   robot.on([
-    'pull_request.reopened',
-    'pull_request.opened',
+    'pull_request',
   ], async context => {
-    robot.log(context)
-    console.log(context)
+    const pr = context.payload.pull_request
 
-    const params = context.issue({body: 'Hello World!'})
+    // If Pull Request isn't closed by merging do nothing.
+    if (context.payload.action !== 'closed' && pr.merged !== true) {
+      return
+    }
 
-    // Post a comment on the issues
-    return context.github.issues.createComment(params)
+    const config = await setupConfig(context)
+    const branchName = pr.head.ref
+
+    // Delete only if branch isn't excluded
+    if (config.exclude.indexOf(branchName) < 0) {
+      const head = {
+        ...context.repo(),
+        ref: `heads/${branchName}`,
+      }
+
+      const response = await deleteBranch(context, head)
+
+      if (response.status === 204 && config.comment) {
+        const commentData = {
+          owner: head.owner,
+          repo: head.repo,
+          number: pr.number,
+          body: `Deleted merged branch **${branchName}**`,
+        }
+
+        return context.github.issues.createComment(commentData)
+      }
+    }
   })
 }
